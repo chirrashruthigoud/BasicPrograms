f49dbe3d228d527d2208fa0129077bcda9e0b668
chirrashruthigoud <chirrashruthigoud4@gmail.com>
2023-02-09 11:43:29 +0530

    username input

---
+++ username.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    public class username
+    {
+        public static void Displayusername()
+        {
+            Console.WriteLine("Enter your name");
+            string username = Console.ReadLine();
+            int val = username.Length;
+            Console.WriteLine("The number character is" + val);
+
+            if (username.Length > 3 && username.Length < 5)
+            {
+                Console.WriteLine("Hello" + username + ",How are you ");
+                Console.WriteLine("Hello {0} , How are you", username);
+
+            }
+            else if(username.Length > 5)
+            {
+                Console.WriteLine("The name is not having minumum 3 char!!");
+
+            }
+        }
+    }
+}
