49ba57c32702974905ad62f79923742d2f121a10
chirrashruthigoud <chirrashruthigoud4@gmail.com>
2023-02-09 11:30:56 +0530

    First program

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using HelloWorld;
+
+namespace BasicProgram
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Primefactorization.CheckPrimefactor();
+           // Harmonic.Harmonicnumber();
+            //Poweroftwo.CalaculateNumber();
+            //Leapyear.LeapyearorNot();
+            // username.Displayusername();
+            // Flipcoin.CalculatePercentage();
+            //Console.WriteLine("Shruthi Welcome to .Net Framwork ");
+
+
+
+            /* int num = 4;
+             double num1 = 1.8;
+             bool val = true;
+             char singleChar = 's';
+             string name = "Shruthi";
+             Console.WriteLine("Integer : " + num);
+             Console.WriteLine("double : " + num1);
+             Console.WriteLine("bool : " + val);
+             Console.WriteLine("singleChar : " + singleChar);
+             Console.WriteLine("String :" + name);
+             int nums = 100;
+             Console.WriteLine(nums);
+             string str = "Shruthi";
+             Console.WriteLine(str + nums);
+             Console.WriteLine("This is {0} number !!!");
+
+             string takeInput = Console.ReadLine();
+             Console.WriteLine("User input :" + takeInput);
+             Console.WriteLine("The name is  {0} {1} {2} {3} ", takeInput, num, num1, val);*/
+        }
+    }
+}
