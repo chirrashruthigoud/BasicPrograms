07eaef14019c7e1ed4092be804cb5c3b65c481ac
chirrashruthigoud <chirrashruthigoud4@gmail.com>
2023-02-09 11:48:23 +0530

    Leapyear problem

---
+++ Leapyear.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    public class Leapyear
+    {
+        public static void LeapyearorNot()
+        {
+            Console.WriteLine("Enter the year : ");
+            string num = Console.ReadLine();
+            int leapYear = Convert.ToInt32(num);
+
+            if (leapYear % 4 == 0 && leapYear % 100 != 0 || leapYear % 400 == 0)
+            {
+                Console.WriteLine("This Is LeapYear " + leapYear);
+            }
+
+
+            else
+            {
+                Console.WriteLine("This is not LeapYear" + leapYear);
+            }
+        }
+    }
+}
