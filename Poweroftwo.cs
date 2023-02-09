51a1fca52c4aa23a533e5b336e911d5012736bcb
chirrashruthigoud <chirrashruthigoud4@gmail.com>
2023-02-09 11:49:41 +0530

    Poweroftwo problem

---
+++ Poweroftwo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    public class Poweroftwo
+    {
+        public static void CalaculateNumber()
+        {
+            int i;
+            int number = 67;
+            int N = 22;
+            for (i = 0; i <= N; i++)
+            {
+                double pow = Math.Pow(number, i);
+                Console.WriteLine("Calculated 2 power" + i + " : " + pow);
+            }
+        }
+    }
+}
